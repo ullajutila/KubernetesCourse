5e5094e9c3279cc2e01aa268b2848309f1c0d3da
Ulla Jutila <jujutila@gmail.com>
2025-07-06 11:33:45 +0300

    fifth

--- the_project/server.py
+++ the_project/server.py
@@ -2,7 +2,10 @@ import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 def main():
-    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT not set
+    port = int(os.environ.get("PORT", 8000))  # Default port if not set
+    web_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(web_dir)  # Ensure server serves from current script directory
+
     server_address = ('', port)
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     print(f"Server started in port {port}", flush=True)
